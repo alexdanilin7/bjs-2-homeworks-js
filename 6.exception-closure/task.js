d0cdd820cfd8dcbc4351c571f6e6aa851cef89e6
andanilin <andanilin7@gmail.com>
2025-03-08 01:54:22 +0200

    Задание 6  Обработка исключений

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(number){
+    let parseNumber = Number.parseFloat(number);
+    if (isNaN(parseNumber)){
+         throw new Error("Невалидное значение");
+    }
+    return parseNumber;   
+}
+
+
+function validateCount(number){
+    try{
+        return parseCount(number);
+    }catch (error)
+    {
+        return error;
+    }
+}
+
+
+class Triangle{
+    constructor(a,b,c){
+        if (!(a + b > c && a + c > b && b + c > a)){
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area(){
+        let p = this.perimeter / 2;
+        return Number(Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c)).toFixed(3));
+    }
+
+}
+
+function getTriangle(a, b, c){
+    try{
+        return new Triangle(a, b, c);
+    }catch(error){
+        return {
+            get perimeter(){
+                return "Ошибка! Треугольник не существует";
+            },
+            get area(){
+                return  "Ошибка! Треугольник не существует";
+            } 
+        }
+    }
+}
